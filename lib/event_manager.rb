8a75ffaf1e8d2e94dd238bda99683baba14c91a4
Daniel Murphy <danielmichaelmurphy@gmail.com>
2020-09-13 00:09:30 -0400

    Updated through Iteration 2. Next step Iteration 3.

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,12 +1,16 @@
-puts 'EventManager initialized!'
+require 'csv'
 
-lines = File.readlines 'event_attendees.csv'
-
-lines.each_with_index do |line, index|
-  next if index.zero?
-
-  columns = line.split(",")
-  name = columns[2]
-  puts name
+def clean_zipcode(zipcode)
+  zipcode.to_s.rjust(5, '0')[0..4]
 end
 
+puts 'EventManager initialized!'
+
+contents = CSV.open 'event_attendees.csv', headers: true, header_converters: :symbol
+contents.each do |row|
+  name = row[:first_name]
+
+  zipcode = clean_zipcode(row[:zipcode])
+  
+  puts "#{name} #{zipcode}"
+end
